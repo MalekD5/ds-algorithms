bcafae9ac6c38d0cc299150079faccdbf6024e99
MalekD5 <106095300+MalekD5@users.noreply.github.com>
2022-10-13 16:12:24 -0700

    foundations/tests: add more insertion sort tests

--- foundations/insertion-sort/InsertionSort.spec.js
+++ foundations/insertion-sort/InsertionSort.spec.js
@@ -1,9 +1,19 @@
 const insertionSort = require('./InsertionSort');
 
 describe('insertionSort', () => {
-    test('sort list correctly using insertion sort', () => {
+    test('insertion sort with an array', () => {
         const list = [5, 2, 4, 6, 1, 3];
         const expected = [1, 2, 3, 4, 5, 6];
         expect(insertionSort(list)).toStrictEqual(expected);
     });
+    test('insertion sort with array of length 2', () => {
+        const list = [2, 1];
+        const expected = [1, 2];
+        expect(insertionSort(list)).toStrictEqual(expected);
+    });
+    test('insertion sort with an array of length 1', () => {
+        const list = [1];
+        const expected = [1];
+        expect(insertionSort(list)).toStrictEqual(expected);
+    })
 });
